a991aa20fbfd599bc8ebf6bf0e4af20f4554e852
Madura Eeswara <madura.madushan@outlook.com>
2019-07-22 15:29:41 +0530

    register and login ui setup

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -17,6 +17,12 @@ const Navbar = ({ title, icon }) => {
         <li>
           <NavLink to='/about'>About</NavLink>
         </li>
+        <li>
+          <NavLink to='/register'>Register</NavLink>
+        </li>
+        <li>
+          <NavLink to='/login'>Login</NavLink>
+        </li>
       </ul>
     </div>
   )
