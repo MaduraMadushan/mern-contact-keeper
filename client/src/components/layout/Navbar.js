adcfa639f0cdbc8ca4c56e986e75694b1b4d7f52
Madura Eeswara <madura.madushan@outlook.com>
2019-07-22 08:37:00 +0530

    setup react routes

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <NavLink to='/'>
+          <i className={icon} /> {title}
+        </NavLink>
+      </h1>
+      <ul>
+        <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/about'>About</NavLink>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+}
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt'
+}
+
+export default Navbar
