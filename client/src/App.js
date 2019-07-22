4ded114aeb9dd0d97f5e592d7b5fea1ac4303426
Madura Eeswara <madura.madushan@outlook.com>
2019-07-22 08:49:59 +0530

    ContactState initial state & provider

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,23 @@ import './App.css'
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+import ContactState from './context/contact/ContactState'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <React.Fragment>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </React.Fragment>
-    </BrowserRouter>
+    <ContactState>
+      <BrowserRouter>
+        <React.Fragment>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </BrowserRouter>
+    </ContactState>
   )
 }
 
