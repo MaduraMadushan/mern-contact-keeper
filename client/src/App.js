33c3761a2f6d4f979a01cc29214e2bee20f1f5a4
Madura Eeswara <madura.madushan@outlook.com>
2019-07-22 13:12:41 +0530

    Auth state setup

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,25 @@ import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import ContactState from './context/contact/ContactState'
+import AuthState from './context/auth/AuthState'
 
 const App = () => {
   return (
-    <ContactState>
-      <BrowserRouter>
-        <React.Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </React.Fragment>
-      </BrowserRouter>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <BrowserRouter>
+          <React.Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
+      </ContactState>
+    </AuthState>
   )
 }
 
